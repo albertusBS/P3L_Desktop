09886b333e5f6e9912cdd8592cb938247734ee07
albertusBS <albertusbagus@gmail.com>
2020-03-11 12:09:09 +0700

    Second Commit

--- Project_P3L/MainWindow.xaml.cs
+++ Project_P3L/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,17 +11,91 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
 
 namespace Project_P3L
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+   
     public partial class MainWindow : Window
     {
+        private string connection;
+        MySqlConnection conn;
+        MySqlCommand cmd;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            try
+            {
+                dbConnection();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.Message, "Connection Lost");
+            }
+        }
+
+        private void dbConnection()
+        {
+            try
+            {
+                connection = "SERVER=localhost;DATABASE=pet_shop;UID=root;PASSWORD=;"; //Database Localhost
+                //connection = "SERVER=192.168.19.140;DATABASE=9152;UID=pp9152;PASSWORD=9152;"; //Database Web Server Atma
+                conn = new MySqlConnection(connection);
+                conn.Open();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        private void TxtUser_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TxtPass_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private bool validateLogin(string name, string password)
+        {
+            cmd = new MySqlCommand();
+            cmd.CommandText = "SELECT * FROM employees WHERE name=@user AND password=@password";
+            cmd.Parameters.AddWithValue("@user", name);
+            cmd.Parameters.AddWithValue("@password", password);
+            //cmd.Connection = conn;
+            MySqlDataReader login = cmd.ExecuteReader();
+
+            return login.Read() ? true : false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = txtUser.Text;
+            string password = txtPass.Text;
+
+            if (username == "" || password == "")
+                MessageBox.Show("Please Fill the Field", "Warning");
+            else
+            {
+                bool r = validateLogin(username, password);
+                if (r)
+                {
+                    MessageBox.Show("Welcome", "Welcome");
+                    Window1 window = new Window1();
+                    this.Close();
+                    window.Show();
+                }
+                else
+                    MessageBox.Show("Invalid Username or Password", "Warning");
+            }
         }
     }
 }
---
+++ Project_P3L/Window1.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Project_P3L
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        MySqlCommand cmd;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextAddress_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBirthdate_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextPhone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private bool AddEmployee(string id, string name, string address, string date, string phoneNumber, string role, string password)
+        {
+            cmd = new MySqlCommand();
+            cmd.CommandText = "INSERT INTO employees VAlUES(@id, @name, @address, @birthdate, @phone, @role, @password;";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@address", address);
+            cmd.Parameters.AddWithValue("@birthdate", date);
+            cmd.Parameters.AddWithValue("@phone", phoneNumber);
+            cmd.Parameters.AddWithValue("@role", role);
+            cmd.Parameters.AddWithValue("@password", password);
+            MySqlDataReader addEmployee = cmd.ExecuteReader();
+
+            return addEmployee.Read() ? true : false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
